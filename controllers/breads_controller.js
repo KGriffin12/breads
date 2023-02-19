9662f13ff21ccebb9edcad08fc6631f00a302399
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-02-19 12:52:06 -0600

    Mongoose helper methods.

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,13 +4,15 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread,
-        title: 'Index Page'
-      }
-    )
+  Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              title: 'Index Page'
+          })
+      })
 })
+
 
 
 module.exports = breads
@@ -29,28 +31,33 @@ breads.get('/:indexArray/edit', (req, res) => {
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex,
+breads.get('/:id', (req, res) => {
+  Bread.findById(req.params.id)
+    .then(foundBread => {
+      res.render('show', {
+        bread: foundBread
+      })
     })
-  } else {
-    res.render('404')
-  }
+    .catch(err => {
+      res.send('404')
+    })
 })
+
 
 // CREATE
 breads.post('/', (req, res) => {
-  console.log(req.body)
-  if(req.body.hasGluten === 'on') {
-    req.body.hasGluten = 'true'
-  } else {
-    req.body.hasGluten = 'false'
+  if(!req.body.image) {
+      req.body.image = undefined 
   }
-  Bread.push(req.body)
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread.create(req.body)
   res.redirect('/breads')
 })
+
 
 // DELETE
 breads.delete('/:indexArray', (req, res) => {
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
+const mongoose = require("mongoose")
 
 
 // CONFIGURATION
@@ -29,6 +26,10 @@ app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 
 
