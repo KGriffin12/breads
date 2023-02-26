8fdd526a4402022b7ff2fd91097529bfc6258699
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-02-26 11:13:51 -0600

    Successfully edit breads.

--- views/show.jsx
+++ views/show.jsx
@@ -8,10 +8,10 @@ function Show ({bread, index}) {
         <Default>
         <h3>{bread.name}</h3>
 
-      <form action={`/breads/${index}?_method=DELETE`} method="POST">
+      <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
       <input type='submit' value="DELETE"/>
       </form>
-      <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+      <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
 
         <p>
           and it
