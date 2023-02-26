2a5fdf97d501f34dbdb376f5f19f4de275d6fad5
Karlie Griffin <karliegriffin@Karlies-Mini.lan>
2023-02-26 11:48:11 -0600

    Custom helper methods.

--- views/show.jsx
+++ views/show.jsx
@@ -11,9 +11,9 @@ function Show ({bread, index}) {
       <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
       <input type='submit' value="DELETE"/>
       </form>
-      <img src={bread.image} alt={bread.name}/>
-        <p>Baked by {bread.baker}</p>
-        <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+      <img src={bread.image} alt={bread.name} />
+        <p>{bread.getBakedBy()}</p>
+        <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>          
         <p>
           and it
           {
